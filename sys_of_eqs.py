6fc39d09580dcbb93f6eef817e01bf0e5355fc96
eghnalgl00 <egehanalgul@gmail.com>
2026-02-23 10:19:04 +0300

    ok

--- sys_of_eqs.py
+++ sys_of_eqs.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-"""
+
 A = np.array([
         [-1, 3],
         [3, 2]
@@ -39,7 +39,7 @@ det_A = np.linalg.det(arr)
 soln = np.linalg.solve(arr,b.reshape(2,1))
 
 print(soln)
-"""
+
 
 A = np.array([[4,-3,1],[2,1,3],[-1,2,-5]])
 b = np.array([-10,0,17])
