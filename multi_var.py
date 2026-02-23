fd170fc20d581f5b169686700cbdd86d793390ac
eghnalgl00 <egehanalgul@gmail.com>
2026-02-23 21:54:36 +0300

    regularized

--- multi_var.py
+++ multi_var.py
@@ -13,13 +13,43 @@ x_max = np.max(x_train, axis=0)
 norm_x = (x_train - mean) / std
 norm_x_2 = (x_train - mean) / (x_max - x_min)
 """
-
+"""
 size = np.array([50, 60, 70, 80, 90])
 bedrooms = np.array([1, 2, 2, 3, 3])
 age = np.array([20, 15, 10, 5, 2])
 
 x_train = np.c_[size, bedrooms, age]
 y_train = np.array([55, 100, 135, 180, 209], dtype=float)
+"""
+size = np.array([
+    40, 45, 50, 55, 60,
+    65, 70, 75, 80, 85,
+    90, 95, 100, 105, 110,
+    115, 120, 125, 130, 135
+])
+
+bedrooms = np.array([
+    1, 1, 2, 2, 2,
+    2, 3, 3, 3, 3,
+    3, 4, 4, 4, 4,
+    4, 5, 5, 5, 5
+])
+
+age = np.array([
+    30, 28, 25, 23, 20,
+    18, 15, 13, 12, 10,
+    9, 8, 7, 6, 5,
+    4, 3, 2, 2, 1
+])
+
+x_train = np.c_[size, bedrooms, age]
+
+y_train = (
+    2.5 * size
+    + 15 * bedrooms
+    - 1.2 * age
+    + 10
+).astype(float)
 mean = np.mean(x_train, axis=0) 
 std  = np.std(x_train, axis=0)  
 x_min = np.min(x_train, axis=0)
@@ -30,17 +60,21 @@ norm_x_2 = (x_train - mean) / (x_max - x_min)
 
 
 
-def compute_cost(x,y,w,b):
+def compute_cost(x,y,w,b,lmd):
     m = x.shape[0]
     f_wb = 0
     total_cost = 0
+    reg_term = 0
     for i in range(m):
         f_wb = np.dot(w,x[i]) + b
         total_cost += (f_wb-y[i])**2
+    for i in range(len(x[0])):
+        reg_term += w[i]**2
+    total_cost += lmd*reg_term
     total_cost = total_cost/(2*m)
     return total_cost
 
-def compute_gradient(x,y,w,b):
+def compute_gradient(x,y,w,b,lmd):
     m = x.shape[0]
     dj_dw = np.zeros(len(x[0]))
     dj_db = 0
@@ -52,6 +86,8 @@ def compute_gradient(x,y,w,b):
         dj_db += (f_wb-y[i])
     dj_dw = dj_dw/m
     dj_db = dj_db/m
+    for i in range(len(dj_dw)):
+        dj_dw[i] += (lmd/m)*w[i]
     return dj_dw , dj_db
 
 def gradient_descent(x,y,w,b,alpha,iters):
@@ -59,9 +95,9 @@ def gradient_descent(x,y,w,b,alpha,iters):
     w_b_hist = []
     dw_db_hist = []
     for i in range(iters):
-        dj_dw , dj_db = compute_gradient(x,y,w,b)
-        cost_hist.append(compute_cost(x, y, w, b))
-        w_b_hist.append((w,b))
+        dj_dw , dj_db = compute_gradient(x,y,w,b,lmd)
+        cost_hist.append(compute_cost(x, y, w, b,lmd))
+        w_b_hist.append((w.copy(),b))
         dw_db_hist.append([dj_dw,dj_db])
         w = w - alpha*dj_dw
         b = b - alpha*dj_db 
@@ -72,21 +108,21 @@ def gradient_descent(x,y,w,b,alpha,iters):
     n = cost_hist.index(min(cost_hist))
     return w_b_hist[n] , min(cost_hist) , len(cost_hist) , alpha , cost_hist , dw_db_hist
 
-w = np.array([600,600,600])
+w = np.array([6,6,6])
 b = 19
 alpha = 3e-1
 iters = 10000
+lmd = 0.6
 
 best_wb, best_cost, steps , alpha_final ,cost_hist , dw_db_hist= gradient_descent(norm_x, y_train, w, b, alpha, iters)
 w_1, b_1 = best_wb
 
 print(best_wb, best_cost, steps , alpha_final)
-def predict(x):
-    out= np.dot(w_1,x) + b_1
-    return out
 
-print(predict([75, 2, 12]))
+def predict_raw(x_raw):
+    x_raw = np.array(x_raw, dtype=float)
+    x_norm = (x_raw - mean) / std
+    return np.dot(w_1, x_norm) + b_1
 
-plt.plot(cost_hist)
-plt.grid(True)
-plt.show()
+
+print(predict_raw([120, 4, 10]))
