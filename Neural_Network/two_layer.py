45d94b57641fcea13f40797ad4c1a0bbd502f010
eghnalgl00 <egehanalgul@gmail.com>
2026-08-19 17:55:07 +0300

    "2nd"

---
+++ Neural_Network/two_layer.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_1 = np.array([0.1,0.02,0.03])
+x_2 = np.array([0.03,0.02,30])
+
+x_train = np.c_[x_1,x_2]
+
+def g(z,n):
+    if n == 0:
+        return 1/(1+np.exp(-z))
+    elif n == 1:
+        return z
+    else:
+        return max(0,z)
+
+def dense(a_in , w ,b ,n):
+    units = w.shape[1]
+    a_out = np.zeros(units)
+    a_out = a_in @ w + b
+        
+    return a_out
+
+
+def sequential(x):
+    a1 = dense(x , w1 ,b1,0)
+    a2 = dense(a1 , w2 ,b2,0)
+    a3 = dense(a2 , w3 ,b3,1)
+    f_x = a3
+    return f_x
+
+
+w1 = np.array([[1,2,3],[4,5,6]]) / 10000000
+w2 = np.array([[1,2,3,4],[4,5,6,7],[7,8,9,10]]) / 1000000
+w3 = np.array([[1],[2],[3],[4]]) / 10000000
+b1 = np.array([1,2,3]) 
+b2 = np.array([1,2,3,4]) 
+b3 = np.array([1]) 
+
+for i, x in enumerate(x_train):
+    print(f"Sample {i}: {sequential(x)}")
