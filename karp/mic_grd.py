01c15986065e2aa62e818d4a637f8fcf3ec79c84
eghnalgl00 <egehanalgul@gmail.com>
2026-08-19 17:52:27 +0300

    2nd time.

--- karp/mic_grd.py
+++ karp/mic_grd.py
@@ -2,29 +2,6 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from graphviz import Digraph
-"""
-def f(x):
-    return x**3 + 5*(x**2) + 3
-
-xs = np.arange(-5,5,0.25)
-ys = f(xs)
-
-plt.plot(xs,ys)
-"""
-"""
-a = 2.0
-b = -3.0
-c = 10.0
-d = a*b + c
-
-
-d1 = a*b+c
-h = 0.00001
-a += h
-d2 = a*b + c
-
-(d2-d1)/h #Slope
-"""
 
 def trace(root):
     # build the set of all nodes and edges in the graph
@@ -120,24 +97,6 @@ class Value:
         for node in reversed(topo):
             node._backward()
 
-"""
-def lol():
-
-    h = 0.0001
-
-    a = Value(2.0 , label = "a")
-    #print(a.data) -> 2.0    
-    b = Value(-3.0 , label = "b")
-    c = a + b ; c.label = "c"  #Value(-1.0)
-    d = a*b + c ; d.label = "d" #Value(-7.0)
-    d._prev #(Value(-6.0) . Value(-1.0))
-    d._op # "+"
-    e = d * c ; e.label = "e"
-    L = e * d ; L.label = "L"
-    L.grad = 1.0
-    e.grad = d.data ; d.grad = e.data
-"""    
-
 x1 = Value(2.0 , label = "x1")
 x2 = Value(0.0 , label = "x2")
 #Weights w1, w2
@@ -152,23 +111,7 @@ x2w2= x2*w2 ; x2w2.label = "x2w2"
 x1w1x2w2 = x1w1 + x2w2 ; x1w1x2w2.label = "x1w1x2w2"
 n = x1w1x2w2 + b ; label = "n"
 o = n.tanh() ; o.label = "o"
-"""
-o.grad = 1.0 #do/do = 1
-# tanh grad : do/dn = 1 - tanh(n)**2
-n.grad = 0.5
-x1w1x2w2 = 0.5 ; b.grad = 0.5 #addition -> grad = 1
-x1w1.grad = 0.5 ; x2w2.grad = 0.5
-x2.grad = w2.data * x2w2.grad ; w2.grad = x2.data * x2w2.grad
-x1.grad = w1.data * x1w1.grad ;w1.grad = x1.data * x1w1.grad
-"""
-"""
-o.grad = 1.0
-o._backward()
-n._backward()
-b._backward()
-x1w1x2w2._backward()
-x2w2._backward()
-x1w1._backward()
-"""
+
 
 o.backward()
+draw_dot(o).render()
